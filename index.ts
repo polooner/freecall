f1a7d52c9cfddcd996a00e3b687e54764e35395f
Filip Wojda <wojdafilipdev@gmail.com>
2023-12-22 13:11:49 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com');
+  // other actions...
+  await browser.close();
+})();
